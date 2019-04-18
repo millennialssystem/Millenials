40a8c58231244cfd6052ceb9dd936dcf54912b03
millennialssystem <millennialssystem@gmail.com>
2019-04-18 13:26:22 -0400

    Cambios para desarrollar

---
+++ MSDeveloper/MSDeveloper/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MSDeveloper.Models;
+
+namespace MSDeveloper.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var configu = new WebSetting()
+            {
+                Id = 1,
+                Name = "jose"
+            };
+            return View(configu);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MSDeveloper/MSDeveloper/Models/WebSetting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSDeveloper.Models
+{
+    public class WebSetting
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
